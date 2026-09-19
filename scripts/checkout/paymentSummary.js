a522e95d8c3fe968f0a1031e76b16a851a56d369
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-09-19 23:55:53 +0530

    Refractor: created getProduct function to be called in the checkout page files implementing reusability

---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,7 @@
+import {cart} from '../../data/cart.js';
+import {getProduct} from '../../data/products.js';
+export function renderPaymentSummary(){
+  cart.forEach((cartItem) => {
+    const matchingProduct = getProduct(cartItem.productId);
+  })
+}
