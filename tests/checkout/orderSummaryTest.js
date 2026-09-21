035e5b08d61da3c26111a15455a9a1a6235449d7
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-09-22 02:12:44 +0530

    test: create unit test in renderOrderSummary test suite to test delivery option updation rendereing and storage

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -65,4 +65,14 @@ describe('test suite: renderOrderSummary', () => {
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
   });
+
+  it('updates delivery option', () => {
+    document.querySelector(`.js-delivery-option-${'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'}-${'3'}`).click();
+
+    expect(document.querySelector(`.js-delivery-option-input-${'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'}-${'3'}`).checked).toEqual(true);
+
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].deliveryOptionId).toEqual('3');
+  });
 });
