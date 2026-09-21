cf0549d7678c6ca4d25d96a59b2bcce54ec4b2d7
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-09-21 22:18:30 +0530

    test: implemeted befroeEach and afterEach hook to clean the test code

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -3,41 +3,8 @@ import {loadFromStorage, cart} from "../../data/cart.js";
 
 
 describe('test suite: renderOrderSummary', () => {
-  it('displaysTheCart', () => {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
-    document.querySelector('.js-test-container').innerHTML = `
-    <div class= js-checkout-header></div>
-    <div class="js-order-summary"></div>
-    `;
-
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: 
-        'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
-        deliveryOptionId: '1'
-      }, {
-        productId:
-        '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 2,
-        deliveryOptionId: '2'
-      }]);
-    });
-    loadFromStorage();
-    renderOrderSummary();
-
-    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
-    expect(
-      document.querySelector(`.js-product-quantity-${'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'}`).innerText
-    ).toContain('Quantity: 1');
-    expect(
-      document.querySelector(`.js-product-quantity-${'15b6fc6f-327a-4ec4-896f-486349e85a3d'}`).innerText
-    ).toContain('Quantity: 2');
-
-    document.querySelector('.js-test-container').innerHTML = ``;
-  });
-  
-  it('removesAProduct', () => {
     document.querySelector('.js-test-container').innerHTML = `
     <div class= js-checkout-header></div>
     <div class="js-order-summary"></div>
@@ -60,7 +27,23 @@ describe('test suite: renderOrderSummary', () => {
     loadFromStorage();
 
     renderOrderSummary();
+  });
 
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = ``;
+  });
+
+  it('displaysTheCart', () => {
+    expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+    expect(
+      document.querySelector(`.js-product-quantity-${'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'}`).innerText
+    ).toContain('Quantity: 1');
+    expect(
+      document.querySelector(`.js-product-quantity-${'15b6fc6f-327a-4ec4-896f-486349e85a3d'}`).innerText
+    ).toContain('Quantity: 2');
+  });
+  
+  it('removesAProduct', () => {
     document.querySelector(`.js-delete-link-${'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'}`).click();
     expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
 
@@ -75,6 +58,4 @@ describe('test suite: renderOrderSummary', () => {
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
   });
-
-  document.querySelector('.js-test-container').innerHTML = ``;
 });
