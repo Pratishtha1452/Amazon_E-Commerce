530ad8360b2b8190985d736d71be97e8bee28845
Pratishtha Deshpande <pratishthadeshpande17@gmail.com>
2026-09-22 20:35:25 +0530

    test: implemented unit test for wwhen the delivery option is invalid

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -123,4 +123,10 @@ describe('test suite: update delivery Option', () =>{
     expect(cart[0].deliveryOptionId).toEqual('1');
     expect(localStorage.setItem).toHaveBeenCalledTimes(0);
   });
+  it('does nothing if deliveryOption does not exist', () => {
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '4');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].deliveryOptionId).toEqual('1');
+  });
 });
